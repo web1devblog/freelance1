6cb4029921785edf67cf624a905444ed8b6606d4
web1devblog <97555209+web1devblog@users.noreply.github.com>
2022-01-11 23:53:28 +0400

    Add files via upload

---
+++ public_html (7)/js/swiper.js
@@ -0,0 +1,12 @@
+new Swiper('.image-slider',{
+    pagination:{
+        el:'.swiper-pagination',
+        clickable: true,
+        
+    },
+    autoplay: {
+        delay: 3500,
+       },
+       loop: true,
+       speed: 1000,
+});
